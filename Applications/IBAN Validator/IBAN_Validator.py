08e41413412fbc394b0b236a75f4e5cafc2f1db0
Nikhil Jatale <nikhiljatale@gmail.com>
2023-02-22 16:46:45 +0000

    .py

---
+++ Applications/IBAN Validator/IBAN_Validator.py
@@ -0,0 +1,32 @@
+# IBAN Validator.
+
+iban = input("Enter IBAN, please: ")
+iban = iban.replace(' ','')
+
+if not iban.isalnum():
+    print("You have entered invalid characters.")
+elif len(iban) < 15:
+    print("IBAN entered is too short.")
+elif len(iban) > 31:
+    print("IBAN entered is too long.")
+else:
+    iban = (iban[4:] + iban[0:4]).upper()
+    iban2 = ''
+    for ch in iban:
+        if ch.isdigit():
+            iban2 += ch
+        else:
+            iban2 += str(10 + ord(ch) - ord('A'))
+    iban = int(iban2)
+    if iban % 97 == 1:
+        print("IBAN entered is valid.")
+    else:
+        print("IBAN entered is invalid.")
+
+
+'''
+Test data
+British: GB72 HBZU 7006 7212 1253 00
+French: FR76 30003 03620 00020216907 50
+German: DE02100100100152517108
+'''
